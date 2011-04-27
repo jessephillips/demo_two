d1476d1d84f3cc6e22dcf94560355776444e266f
Jesse Phillips <mr.jesse.phillips@gmail.com>
2011-04-26 21:31:53 -0400

    Added Microposts resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110426164953) do
+ActiveRecord::Schema.define(:version => 20110427013104) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
